0ff9de4420a4fc9d3c6d7d64f713210516f0a222
Robin Spinks <uglybluecat@googlemail.com>
2016-09-07 12:44:58 +0100

    Add NetworkManager singleton

---
+++ IGTDevTask/IGTDevTask/NetworkManager.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkManager.swift
+//  IGTDevTask
+//
+//  Created by Robin Spinks on 07/09/2016.
+//  Copyright © 2016 UglyBlueCat. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    /*
+     * Create a shared instance to initialise class as a singleton
+     * (originally taken from: http://krakendev.io/blog/the-right-way-to-write-a-singleton)
+     */
+    static let sharedInstance = NetworkManager()
+    private init() {}
+
+}

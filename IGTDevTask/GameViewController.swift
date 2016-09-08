3b3baf9e8655cd0f0f63f1bf64842dd088c4c71a
Robin Spinks <uglybluecat@googlemail.com>
2016-09-08 19:50:48 +0100

    view controllers

--- IGTDevTask/GameViewController.swift
+++ IGTDevTask/GameViewController.swift
@@ -1,32 +1,23 @@
 //
-//  ViewController.swift
+//  GameViewController.swift
 //  IGTDevTask
 //
-//  Created by Robin Spinks on 06/09/2016.
+//  Created by Robin Spinks on 08/09/2016.
 //  Copyright © 2016 UglyBlueCat. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class GameViewController: UIViewController {
     
-    var titleLabel: UILabel!
-    var resultsTable: UITableView!
-
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(newDataRecieved), name: kNewData, object: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
+    
     override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
         positionObjectsWithinSize(size)
-        resultsTable.reloadData()
     }
     
     func setupView () {
@@ -41,9 +32,4 @@ class ViewController: UIViewController {
     func positionObjectsWithinSize (size: CGSize) {
         
     }
-    
-    func newDataRecieved () {
-        resultsTable.reloadData()
-    }
 }
-

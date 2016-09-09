89df98830224e16f7da2aa887b0dccffb19277a7
Robin Spinks <uglybluecat@googlemail.com>
2016-09-09 10:09:38 +0100

    fixed file creation issue

--- IGTDevTask/FileHandler.swift
+++ IGTDevTask/FileHandler.swift
@@ -12,14 +12,19 @@ class FileHandler {
     
     let fileManager : NSFileManager = NSFileManager.defaultManager()
     let gamesFile : String = "gamesFile.txt"
-    var gamesFilePath : NSURL
+    var gamesFilePath : String
     
     static let sharedInstance = FileHandler()
     
     private init() {
         let paths : [String] = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
         let documentsDirectory : NSURL = NSURL(fileURLWithPath: paths[0])
-        gamesFilePath = documentsDirectory.URLByAppendingPathComponent(gamesFile)
+        if let filePath = documentsDirectory.URLByAppendingPathComponent(gamesFile).relativePath {
+            gamesFilePath = filePath
+        } else {
+            DLog("Cannot construct games file path")
+            gamesFilePath = ""
+        }
     }
     
     /*
@@ -31,8 +36,8 @@ class FileHandler {
      */
     func writeDataToFile (data: NSData) {
         	
-        guard fileManager.createFileAtPath(gamesFilePath.absoluteString, contents: data, attributes: nil) else {
-            DLog("Cannot create file: \(gamesFilePath.absoluteString)")
+        guard fileManager.createFileAtPath(gamesFilePath, contents: data, attributes: nil) else {
+            DLog("Cannot create file: \(gamesFilePath)")
             return
         }
         readDataFromFile()
@@ -46,14 +51,14 @@ class FileHandler {
      * some ideas for date comparison taken from http://stackoverflow.com/questions/26198526/nsdate-comparison-using-swift
      */
     func fileValid () -> Bool {
-        guard fileManager.fileExistsAtPath(gamesFilePath.absoluteString) else {
+        guard fileManager.fileExistsAtPath(gamesFilePath) else {
             return false
         }
         
         var fileAttributes : NSDictionary = NSDictionary()
         
         do {
-            fileAttributes = try fileManager.attributesOfItemAtPath(gamesFilePath.absoluteString)
+            fileAttributes = try fileManager.attributesOfItemAtPath(gamesFilePath)
         } catch {
             DLog("Error accessing file attributes: \(error)")
             return false
@@ -81,7 +86,7 @@ class FileHandler {
      */
     func readDataFromFile () {
         if fileValid(),
-           let data : NSData = fileManager.contentsAtPath(gamesFilePath.absoluteString) {
+           let data : NSData = fileManager.contentsAtPath(gamesFilePath) {
                DataHandler.sharedInstance.handleJackpotData(data)
         } else {
             NetworkManager.sharedInstance.fetchJackpotList()
